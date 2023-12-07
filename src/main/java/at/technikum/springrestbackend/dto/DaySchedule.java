ee248626683178786b487c3f73c231ee6d522ba2
Evan Roussin <evan.roussin@gmail.com>
2023-12-07 05:45:12 +0100

    Add DTO TimeSlot and GeneralAvailability to TimeSlot & few modifications

--- src/main/java/at/technikum/springrestbackend/dto/DaySchedule.java
+++ src/main/java/at/technikum/springrestbackend/dto/DaySchedule.java
@@ -3,11 +3,10 @@ package at.technikum.springrestbackend.dto;
 import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 @AllArgsConstructor
 public class DaySchedule {
     LocalDate day;
-    List<LocalTime> availableTimes;
+    List<TimeSlot> availableTimes;
 }
