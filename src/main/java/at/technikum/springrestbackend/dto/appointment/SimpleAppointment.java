e7e4982fa0abf3fc8fe11c6107240fa78473e746
eroussin <evan.roussin@gmail.com>
2024-01-24 17:37:17 +0100

    More info for DTO

--- src/main/java/at/technikum/springrestbackend/dto/appointment/SimpleAppointment.java
+++ src/main/java/at/technikum/springrestbackend/dto/appointment/SimpleAppointment.java
@@ -12,6 +12,8 @@ public class SimpleAppointment {
     private UUID id;
     private UUID lawyerId;
     private String lawyerName;
+    private UUID userId;
+    private String userName;
     private String date;
     private String time;
 
@@ -19,6 +21,8 @@ public class SimpleAppointment {
         this.id = appointment.getId();
         this.lawyerId = appointment.getLawyer().getId();
         this.lawyerName = appointment.getLawyer().getFirstName() + " " + appointment.getLawyer().getLastName();
+        this.userId = appointment.getUser().getId();
+        this.userName = appointment.getUser().getFirstName() + " " + appointment.getUser().getLastName();
         this.date = appointment.getDate().toString();
         this.time = appointment.getStartTime().toString();
     }
